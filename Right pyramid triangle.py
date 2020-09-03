2ce27e9220a45ddbdadd31fcc24a7009f6591544
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-09-03 21:17:01 +0530

    Update Right pyramid triangle.py

--- Right pyramid triangle.py
+++ Right pyramid triangle.py
@@ -1,14 +1,13 @@
 n=int(input())
 for i in range(0,n):
-   # i+1 is initial value to print first * and n+1 is termination value
-  for j in range(0,i+1):
-    print("*",end=" ")
+   for j in range(0,i+1):
+      print("*",end=" ")
   print("\r")
 
-
-#output- 5
-* * * * * 
-* * * * 
-* * * 
-* * 
+#Output- 
+5
 * 
+* * 
+* * * 
+* * * * 
+* * * * * 
